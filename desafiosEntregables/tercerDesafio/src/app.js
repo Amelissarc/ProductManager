1f2836248009d8475197520e3813e0d1d714129d
Amelissarc <melissa.rinconft@gmail.com>
2023-05-15 21:42:35 -0300

    tercerDesafio

--- desafiosEntregables/tercerDesafio/src/app.js
+++ desafiosEntregables/tercerDesafio/src/app.js
@@ -7,55 +7,86 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-const productManager = new ProductManager();
+const productManager = new ProductManager('./files/products.json');
 
-// Inicializar ProductManager y cargar los productos desde el archivo
-productManager.initialize().then(() => {
-  app.get('/products', async (req, res) => {
-    const products = await productManager.getProducts();
+// Middleware para inicializar ProductManager antes de procesar las solicitudes
+app.use(async (req, res, next) => {
+  try {
+    await productManager.initialize();
+    next();
+  } catch (error) {
+    console.log('Error al inicializar ProductManager:', error);
+    res.status(500).send('Error al inicializar ProductManager');
+  }
+});
+
+// Obtener todos los productos
+app.get('/products', (req, res) => {
+  const products = productManager.getProducts();
+  res.send(products);
+});
+
+// Filtrar productos por precio
+app.get('/filter', (req, res) => {
+  const price = req.query.price;
+
+  if (!price || (price !== '15.99' && price !== '11.99')) {
+    const products = productManager.getProducts();
     res.send(products);
-  });
-
-  // Definir ruta para filtrar productos por precio
-  app.get('/filter', (req, res) => {
-    const price = req.query.price;
-
-    if (!price || (price !== '15.99' && price !== '11.99')) {
-      return res.send(productManager.getProducts());
-    }
-
+  } else {
     const priceFilter = productManager.getProducts().filter(product => product.price === parseFloat(price));
     res.send(priceFilter);
-  });
-
-  // Definir ruta para obtener un producto por su código
-  app.get('/code/:id', (req, res) => {
-    const id = req.params.id;
-
-    const product = productManager.getProducts().find(product => product.code === id);
-
-    if (!product) {
-      return res.status(404).send('Producto no encontrado');
-    }
-
-    res.send(product);
-  });
-
-  app.listen(port, () => {
-    console.log(`El servidor está en marcha y funcionando en el puerto ${port}`);
-  });
-}).catch(error => {
-  console.log('Error al inicializar ProductManager:', error);
+  }
 });
 
-// Middleware para capturar errores no controlados
-app.use((err, req, res, next) => {
-  console.error('Error:', err);
-  res.status(500).send('Ha ocurrido un error en el servidor');
+// Obtener producto por ID
+app.get('/product/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const product = productManager.getProductById(id);
+
+  if (!product) {
+    return res.status(404).send('Producto no encontrado');
+  }
+
+  res.send(product);
 });
 
-productManager.initialize().then(() => {
-  // Resto del código
-}).catch(error => {
-  console.log('Error al inicializar ProductManager:', error);
-});  
+// Agregar un nuevo producto
+app.post('/product', (req, res) => {
+  const product = req.body;
+  productManager.addProduct(product);
+  res.send('Producto agregado exitosamente');
+});
+
+// Actualizar un producto existente
+app.put('/product/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const newProduct = req.body;
+  productManager.updateProduct(id, newProduct);
+  res.send('Producto actualizado exitosamente');
+});
+
+// Eliminar un producto
+app.delete('/product/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  productManager.deleteProduct(id);
+  res.send('Producto eliminado exitosamente');
+});
+
+app.listen(port, () => {
+  console.log(`El servidor está en marcha y funcionando en el puerto ${port}`);
+});
+
+
+
+/* nodemon app.js
+
+Obtener todos los productos: GET /products
+Filtrar productos por precio: GET /filter?price=precio
+Obtener un producto por ID: GET /product/id
+Agregar un nuevo producto: POST /product
+Actualizar un producto existente: PUT /product/id
+Eliminar un producto: DELETE /product/id
+
+*/
