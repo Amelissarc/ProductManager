9553e1b963367ecc757c4499e09b9f93ae1f4c72
Amelissarc <melissa.rinconft@gmail.com>
2023-06-05 23:07:09 -0300

    add socket.io and handlebars

--- Entregas/PreEntrega1/src/routes/products.router.js
+++ Entregas/PreEntrega1/src/routes/products.router.js
@@ -1,11 +1,9 @@
 import { Router } from 'express';
-import fs from 'fs';
 import path from 'path';
-import ManagerProducts from '../clasess/ProductsManager.class.js';
 import { fileURLToPath } from 'url';
+import ManagerProducts from '../clasess/ProductsManager.class.js';
 
 const router = Router();
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const filePath = path.join(__dirname, '../classes/files/products.json');
--- Entregas/PreEntrega1/src/server.js
+++ Entregas/PreEntrega1/src/server.js
@@ -1,18 +1,39 @@
 import express from 'express';
+import exphbs from 'express-handlebars';
+import http from 'http';
+import { Server } from 'socket.io';
 import routerProducts from './routes/products.router.js';
 import routerCart from './routes/carts.router.js';
 
 const app = express();
 const port = 8080;
 
+// Configurar Handlebars
+const hbs = exphbs.create();
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Configurar Socket.io
+const server = http.createServer(app);
+const io = new Server(server);
+
+io.on('connection', (socket) => {
+  console.log('Nuevo cliente conectado');
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
+  });
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', routerProducts);
 app.use('/', routerCart);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`El servidor está en marcha y funcionando en el puerto ${port}`);
 });
+
 
 /* nodemon app.js
 Obtener todos los productos: GET /api/products
