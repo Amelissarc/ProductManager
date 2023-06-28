492d3d893ad8099fe87eded262615cebe0e8e707
Amelissarc <melissa.rinconft@gmail.com>
2023-06-28 20:58:05 -0300

    segundaPreEntrega

--- Entregas/PreEntrega1/src/routes/carts.router.js
+++ Entregas/PreEntrega1/src/routes/carts.router.js
@@ -10,7 +10,7 @@ let managerCarts = new ManagerCarts();
 router.get("/:cid", async (req, res) => {
   let id = req.params.cid;
 
-  let cart = await managerCarts.getCartById(id);
+  let cart = await managerCarts.getCartById(id).populate("products"); 
 
   if (!cart) {
     res.send("No se encontró el carrito");
@@ -55,11 +55,38 @@ router.delete("/:cid/product/:pid", async (req, res) => {
   res.send({ status: "success" });
 });
 
+router.put("/:cid", async (req, res) => { 
+  let cartId = req.params.cid;
+  let products = req.body.products;
+
+  await managerCarts.updateCart(cartId, products);
+
+  res.send({ status: "success" });
+});
+
+router.put("/:cid/products/:pid", async (req, res) => { 
+  let cartId = req.params.cid;
+  let productId = req.params.pid;
+  let quantity = req.body.quantity;
+
+  await managerCarts.updateProductQuantity(cartId, productId, quantity);
+
+  res.send({ status: "success" });
+});
+
 router.delete("/:cid", async (req, res) => {
   let cartId = req.params.cid;
   await managerCarts.deleteAllProductsFromCart(cartId);
   res.send({ status: "success" });
 });
 
-export default router;
+router.delete("/:cid/products/:pid", async (req, res) => { 
+  let cartId = req.params.cid;
+  let productId = req.params.pid;
 
+  await managerCarts.deleteProductFromCart(cartId, productId);
+
+  res.send({ status: "success" });
+});
+
+export default router;
